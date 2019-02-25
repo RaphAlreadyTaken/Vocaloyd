9e61f2ab9b9323e30720b6336de8e00d16df9838
Raphaël MONTET <raflegrand@hotmail.com>
2019-02-25 12:42:01 +0100

    sync94654165
    we're getting there

--- STTServer/server.js
+++ STTServer/server.js
@@ -1,14 +1,15 @@
-/******** Middlewares
+/******** Middleware
 *
 ********/
-const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
+const express = require('express');
+const formidable = require('express-formidable');
+var fs = require('fs');
+var http = require('http');
+const MongoClient = require('mongodb').MongoClient;
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const MongoClient = require('mongodb').MongoClient;
-var http = require('http');
-var fs = require('fs');
+const path = require('path');
 
 /******** Variables
  *
@@ -21,16 +22,35 @@ const port = 3131;
  *
  ********/
 var index = require('./VocaloydSTT/business/routes/index');
-var log = require('./VocaloydSTT/business/routes/log')
+var log = require('./VocaloydSTT/business/routes/log');
 var transcribe = require('./VocaloydSTT/business/routes/transcribe');
 
 /******** Includes
  *
  ********/
+app.use(session(
+{
+    secret: 'vocaloyd in da place',
+    saveUninitialized: false,
+    resave: false,
+    store: new MongoDBStore(
+    {
+        uri: "mongodb://localhost:27017/db",
+        collection: 'vocaloydSessions',
+        touchAfter: 24 * 3600
+    }),
+    cookie: {maxAge: 24 * 3600 * 1000}
+}));
+
+app.use(express.static(path.join(__dirname, './VocaloydSTT/business'))); //Ajout répertoire business dans "path" de l'app
+app.use(express.static(path.join(__dirname, './VocaloydSTT/scss'))); //Ajout répertoire scss dans "path" de l'app
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({limit: '10mb'}));
+app.use(formidable());
 app.use('/', index);    //index.js import
+app.use('/log', log);    //log.js import
 app.use('/transcribe', transcribe); //transcribe.js import
+
 
 /******** Server config
  *
